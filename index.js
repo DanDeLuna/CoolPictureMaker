e2e8f81c01045b165c4f5ee79c79db109b5e7201
DanDeLuna <danieldeluna4@gmail.com>
2023-04-27 21:09:10 -0600

    Added question structure

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const inquirer= require('inquirer') ;
+const //shape = ;
+
+
+//questions 
+const questions = [
+    {
+        type:'list',
+        name:'',
+        message:'',
+    },
+    {
+        type:'',
+        name:'',
+        message:'',
+    },
+    {
+        type:'',
+        name:'',
+        message:'',
+    },
+];    
